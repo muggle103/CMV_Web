bc77d2854f4aa6b65d9bc6d06127d3efe7395e9e
muggle103 <muggle103@gmail.com>
2017-06-06 20:49:08 -0600

    Deleted unnecessary files.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	skel.breakpoints({
@@ -300,7 +294,7 @@
 
 			$banner
 				._parallax();
-		// ===== Scroll to Top ==== 
+		// ===== Scroll to Top ====
 		$(window).scroll(function() {
 		    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
 		        $('#return-to-top').fadeIn(200);    // Fade in the arrow
